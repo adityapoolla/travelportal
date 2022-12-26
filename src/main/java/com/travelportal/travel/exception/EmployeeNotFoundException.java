2969bbb7d642658636869ff0182145d4e563541f
Aditya Poolla <poollaad19@gmail.com>
2022-12-26 15:50:20 +0530

    first commit

---
+++ src/main/java/com/travelportal/travel/exception/EmployeeNotFoundException.java
@@ -0,0 +1,2 @@
+package com.travelportal.travel.constants;public class EmployeeNotFoundException {
+}
---
+++ src/test/java/com/travelportal/travel/com/travelportal/travel/controller/EmployeeControllerTest.java
@@ -0,0 +1,2 @@
+package com.travelportal.travel.com.travelportal.travel.controller;public class EmployeeControllerTest {
+}
